b4f76b953e2df7d833654ea8ce5d20ef8bc63595
Ash Laidlaw and Dylan Stackhouse <ash@>
2017-01-18 09:57:19 -0800

    add check for valid leap year with proper output

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
 $(function(){
-  var year;
+
 
   $("form").submit(function(event){
 
     event.preventDefault();
-    year = parseInt($("input").val());
+    var year = parseInt($("input").val());
+    var today = new Date();
+    today = today.getFullYear();
+
+    if (year < today) {
+      $(".tense").text("was ")
+    } else if (year > today) {
+      $(".tense").text("will be ")
+    } else {
+      $(".tense").text("is ")
+    }
 
     if($("input").val() && year){
-
+      $("#year_output").show();
       $(".year").text(year);
+
+      if (year % 4 === 0 && year % 100 !== 0 ) {
+        $(".check").text("");
+
+      } else if (year % 400 === 0) {
+        $(".check").text("");
+      } else if(year > today) {
+        $(".tense").text("will ");
+        $(".check").text("not be ");
+      } else {
+        $(".check").text("not ");
+      }
     } else {
       alert("Enter a valid year.");
     }
-
-
-
-
   });
-
-
 });
