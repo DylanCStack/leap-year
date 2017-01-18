dd3ccb5d130585d5ad2817ca4c8f609b2300adc6
Ash Laidlaw and Dylan Stackhouse <ash@>
2017-01-18 09:20:02 -0800

    add check to input to validate year value

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(function(){
   var year;
+
   $("form").submit(function(event){
 
     event.preventDefault();
-    
+    year = parseInt($("input").val());
 
-    if($("input").val()){
-      year = parseInt($("input").val());
+    if($("input").val() && year){
 
       $(".year").text(year);
     } else {
-      alert("Enter something");
+      alert("Enter a valid year.");
     }
 
 
